0c4657162b85330caa24009ee6d249ed1fc59e48
Team5499 <bhsrobot@gmail.com>
2019-01-09 16:47:52 -0800

    finished path action (untested)

--- src/main/kotlin/org/team5499/frc2019/actions/PathAction.kt
+++ src/main/kotlin/org/team5499/frc2019/actions/PathAction.kt
@@ -14,10 +14,25 @@ public class PathAction(
     subsystemsManager: SubsystemsManager
 ) : Action(timeoutseconds) {
 
+    // Keep a reference to the drivetrain locally for simplicity's sake
     private val mDrivetrain: Drivetrain = subsystemsManager.drivetrain
+
+    // The actuall class from MonkeyLib that does all the math for path following
     private val mPathfollower: PathFollower = PathFollower(path,
                             Constants.Dimensions.WHEEL_BASE, Constants.LOOKAHEAD_DISTANCE)
 
+    // Called every tick
     public override fun update() {
+
+        // Get the required motor velocity for the current position on the path
+        val pathfolloweroutput: PathFollower.PathFollowerOutput = mPathfollower.update(mDrivetrain.pose)
+
+        // Set the drivetrain to the right velocity
+        mDrivetrain.setVelocity(pathfolloweroutput.leftVelocity, pathfolloweroutput.rightVelocity)
+    }
+
+    // Returns true if we are ready to move on to the next action
+    public override fun next(): Boolean {
+        return (super.next() || mPathfollower.doneWithPath(mDrivetrain.pose))
     }
 }
