a9305e22a0ce52e3627485652d1a357131dbdaff
Ben Scholar <bens@digitalman.com>
2019-01-12 19:22:58 -0800

    made drivetrain testable

--- src/main/kotlin/org/team5499/frc2019/Robot.kt
+++ src/main/kotlin/org/team5499/frc2019/Robot.kt
@@ -3,6 +3,9 @@ package org.team5499.frc2019
 import edu.wpi.first.wpilibj.TimedRobot
 import edu.wpi.first.wpilibj.XboxController
 
+import org.team5499.monkeyLib.hardware.LazyTalonSRX
+import org.team5499.monkeyLib.hardware.LazyVictorSPX
+
 import org.team5499.frc2019.subsystems.SubsystemsManager
 import org.team5499.frc2019.subsystems.Drivetrain
 import org.team5499.frc2019.subsystems.Lift
@@ -11,11 +14,24 @@ import org.team5499.frc2019.subsystems.Vision
 import org.team5499.frc2019.controllers.SandstormController
 import org.team5499.frc2019.controllers.TeleopController
 
+import com.ctre.phoenix.sensors.PigeonIMU
+
 class Robot : TimedRobot(Constants.ROBOT_UPDATE_PERIOD) {
 
     // inputs
     private val mDriver: XboxController
     private val mCodriver: XboxController
+
+    // hardware
+    private val mLeftMaster: LazyTalonSRX
+    private val mLeftSlave1: LazyVictorSPX
+    private val mLeftSlave2: LazyVictorSPX
+
+    private val mRightMaster: LazyTalonSRX
+    private val mRightSlave1: LazyVictorSPX
+    private val mRightSlave2: LazyVictorSPX
+
+    private val mGyro: PigeonIMU
 
     // subsystems
     private val mDrivetrain: Drivetrain
@@ -29,15 +45,33 @@ class Robot : TimedRobot(Constants.ROBOT_UPDATE_PERIOD) {
     private val mTeleopController: TeleopController
 
     init {
+        // input init
         mDriver = XboxController(Constants.Input.DRIVER_PORT)
         mCodriver = XboxController(Constants.Input.CODRIVER_PORT)
 
-        mDrivetrain = Drivetrain()
+        // hardware init
+        mLeftMaster = LazyTalonSRX(Constants.HardwarePorts.LEFT_DRIVE_MASTER)
+        mLeftSlave1 = LazyVictorSPX(Constants.HardwarePorts.LEFT_DRIVE_SLAVE1)
+        mLeftSlave2 = LazyVictorSPX(Constants.HardwarePorts.LEFT_DRIVE_SLAVE2)
+
+        mRightMaster = LazyTalonSRX(Constants.HardwarePorts.LEFT_DRIVE_MASTER)
+        mRightSlave1 = LazyVictorSPX(Constants.HardwarePorts.LEFT_DRIVE_SLAVE1)
+        mRightSlave2 = LazyVictorSPX(Constants.HardwarePorts.LEFT_DRIVE_SLAVE2)
+
+        mGyro = PigeonIMU(Constants.HardwarePorts.GYRO_PORT)
+
+        // subsystem init
+        mDrivetrain = Drivetrain(
+            mLeftMaster, mLeftSlave1, mLeftSlave2,
+            mRightMaster, mRightSlave1, mRightSlave2,
+            mGyro
+        )
         mLift = Lift()
         mIntake = Intake()
         mVision = Vision()
         mSubsystemsManager = SubsystemsManager(mDrivetrain, mLift, mIntake, mVision)
 
+        // controllers init
         mSandstormController = SandstormController(mSubsystemsManager, mDriver, mCodriver)
         mTeleopController = TeleopController(mSubsystemsManager, mDriver, mCodriver)
     }
