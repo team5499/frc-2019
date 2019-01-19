797ba95876469d3b8e7166eb8dadbd81d68f9b16
Team5499Admin <43303842+FRCTeam5499@users.noreply.github.com>
2019-01-18 18:47:11 -0800

    Auto actions (#9)
    * lots of methods added to drivetrain
    
    * almost done, need to resolve feedback sensor issue
    
    * end of day
    
    * space:
    
    * added lazy victors and added newer version of monkeyLib
    
    * figuring out main
    
    * started working on path action
    
    * lift inital
    
    * finished path action (untested)
    
    * added basic documetation to path action
    
    * started working on turn action
    
    * initial wrist
    
    * updated subsystems manager, robot, other fixes
    
    * finished turn action
    
    * finished turn action
    
    * Created liftAction class
    
    * worked on lift action
    
    * fixed compile errors arising from using a deeky computer without Gradle
    
    * modified actions to be directly passed the drive train or equivilent
    
    * Added wrist, x boundary and y boundary actions
    
    * updated Crossed X and Y boundry actions to work with new system of directly passing drivetrain
    
    * made drive straight action
    
    * coded CrossedIntoBoxAction
    
    * fixed up documentation for action classes
    
    * updated actions to standard par Ben's request
    
    * Update CrossedYBoundaryAction.kt
    
    * Update PathAction.kt
    
    * Update TurnAction.kt
    
    * Update DriveStraightAction.kt
    
    camel case
    
    * Update LiftAction.kt
    
    Variable naming
    
    * Update DriveStraightAction.kt
    
    * finished up
    
    * fixed pre commit
    
    * got rid of old java file

--- src/main/kotlin/org/team5499/frc2019/subsystems/Lift.kt
+++ src/main/kotlin/org/team5499/frc2019/subsystems/Lift.kt
@@ -2,23 +2,111 @@ package org.team5499.frc2019.subsystems
 
 import org.team5499.monkeyLib.Subsystem
 import org.team5499.monkeyLib.hardware.LazyTalonSRX
+import org.team5499.monkeyLib.util.Utils
+
+import com.ctre.phoenix.motorcontrol.InvertType
+import com.ctre.phoenix.motorcontrol.ControlMode
+import com.ctre.phoenix.motorcontrol.NeutralMode
+import com.ctre.phoenix.motorcontrol.FeedbackDevice
 
 import org.team5499.frc2019.Constants
 
+@SuppressWarnings("MagicNumber")
 public class Lift : Subsystem() {
+
+    companion object {
+        private const val kElevatorSlot = 0
+        private const val kMaxElevatorTicks = 1000 // check this
+        private const val kMinElevatorTicks = 0 // check this
+        private const val kTicksPerInch = 1024 // check this
+    }
+
+    public enum class ElevatorMode {
+        OPEN_LOOP,
+        MOTION_MAGIC,
+        ZERO
+    }
 
     private val mMaster: LazyTalonSRX
     private val mSlave: LazyTalonSRX
 
+    private var mElevatorMode: ElevatorMode
+
+    public var zeroed: Boolean
+
+    public val positionInches: Double // inches
+        get() = (mMaster.getSelectedSensorPosition(0) / kTicksPerInch.toDouble()).toDouble()
+
+    public val velocity: Double // inches / second
+        get() = Utils.encoderTicksPer100MsToInchesPerSecond(
+            Constants.Units.ENCODER_TICKS_PER_ROTATION,
+            Constants.Dimensions.SPROCKET_CIR_LIFT,
+            mMaster.sensorCollection.quadratureVelocity
+        )
+
+    public val positionError: Double
+        get() = Utils.encoderTicksToInches(
+            Constants.Units.ENCODER_TICKS_PER_ROTATION,
+            Constants.Dimensions.SPROCKET_CIR_LIFT,
+            mMaster.getClosedLoopError(0)
+        )
+
     init {
-        mMaster = LazyTalonSRX(Constants.HardwarePorts.LIFT_MASTER)
-        mSlave = LazyTalonSRX(Constants.HardwarePorts.LIFT_SLAVE)
+        mMaster = LazyTalonSRX(Constants.HardwarePorts.LIFT_MASTER).apply {
+            configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative, 0, 10)
+            setSensorPhase(false) // check
+            setNeutralMode(NeutralMode.Coast)
+            setInverted(false) // check this
+
+            config_kP(kElevatorSlot, 0.0, 10)
+            config_kI(kElevatorSlot, 0.0, 10)
+            config_kD(kElevatorSlot, 0.0, 10)
+            config_kF(kElevatorSlot, 0.0, 10)
+            configMotionCruiseVelocity(0, 10)
+            configMotionAcceleration(0, 10)
+            selectProfileSlot(kElevatorSlot, 0)
+
+            enableCurrentLimit(true)
+            configPeakCurrentDuration(0, 10)
+            configPeakCurrentLimit(0, 10)
+            configContinuousCurrentLimit(25, 10) // amps
+            enableVoltageCompensation(false)
+            configForwardSoftLimitThreshold(kMaxElevatorTicks, 10)
+            configReverseSoftLimitThreshold(kMinElevatorTicks, 10)
+            configForwardSoftLimitEnable(true, 10)
+            configReverseSoftLimitEnable(true, 10)
+        }
+
+        mSlave = LazyTalonSRX(Constants.HardwarePorts.LIFT_SLAVE).apply {
+            follow(mMaster)
+            setInverted(InvertType.FollowMaster)
+        }
+
+        mElevatorMode = ElevatorMode.ZERO
+        zeroed = false
+
+        mMaster.set(ControlMode.PercentOutput, 0.0)
+    }
+
+    public fun setPower(power: Double) {
+        mElevatorMode = ElevatorMode.OPEN_LOOP
+        val limitedPower = Utils.limit(power, -0.6, 1.0)
+        mMaster.set(ControlMode.PercentOutput, power)
+    }
+
+    public fun setPosition(positionInches: Double) {
+        mElevatorMode = ElevatorMode.MOTION_MAGIC
+        var positionTicks = positionInches * kTicksPerInch
+        positionTicks = Utils.limit(positionTicks, kMinElevatorTicks.toDouble(), kMaxElevatorTicks.toDouble())
+        mMaster.set(ControlMode.MotionMagic, positionTicks)
     }
 
     public override fun update() {
     }
 
     public override fun stop() {
+        setPower(0.0)
+        mMaster.neutralOutput()
     }
 
     public override fun reset() {
---
+++ src/main/kotlin/org/team5499/frc2019/subsystems/Wrist.kt
@@ -0,0 +1,89 @@
+package org.team5499.frc2019.subsystems
+
+import org.team5499.monkeyLib.util.Utils
+import org.team5499.monkeyLib.Subsystem
+import org.team5499.monkeyLib.hardware.LazyTalonSRX
+
+import org.team5499.frc2019.Constants
+
+import com.ctre.phoenix.motorcontrol.ControlMode
+import com.ctre.phoenix.motorcontrol.FeedbackDevice
+import com.ctre.phoenix.motorcontrol.NeutralMode
+
+@SuppressWarnings("MagicNumber")
+
+public class Wrist : Subsystem() {
+
+    private val mMaster: LazyTalonSRX
+
+    companion object {
+        private const val kWristSlot = 0
+        private const val kMaxWristTicks = Constants.Units.ENCODER_TICKS_PER_ROTATION / 4
+        private const val kMinWristTicks = 0
+        private const val kTicksPerDegree = Constants.Units.ENCODER_TICKS_PER_ROTATION / 360
+    }
+
+    public val wristAngle: Double // degrees
+        get() = (mMaster.getSelectedSensorPosition(0) / kTicksPerDegree.toDouble()).toDouble()
+
+    public val velocity: Double = 0.0 // degrees / second
+        // INSERT GET FUNCTION TO GET VELOCITY OF WRIST
+
+    public val angleError: Double = 0.0 // degrees
+        // INSERT GET FUNCTION TO GET ERROR OF WRIST
+
+    init {
+        mMaster = LazyTalonSRX(Constants.HardwarePorts.WRIST_MASTER).apply {
+            configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative, 0, 10)
+
+            setSensorPhase(false) // check
+            setNeutralMode(NeutralMode.Coast)
+            setInverted(false) // check this
+
+            config_kP(kWristSlot, 0.0, 10)
+            config_kI(kWristSlot, 0.0, 10)
+            config_kD(kWristSlot, 0.0, 10)
+            config_kF(kWristSlot, 0.0, 10)
+            configMotionCruiseVelocity(0, 10) // check these
+            configMotionAcceleration(0, 10)
+            selectProfileSlot(kWristSlot, 0)
+
+            enableCurrentLimit(true)
+            configPeakCurrentDuration(0, 10)
+            configPeakCurrentLimit(0, 10)
+            configContinuousCurrentLimit(25, 10)
+            enableVoltageCompensation(false)
+            configForwardSoftLimitThreshold(kMaxWristTicks, 10)
+            configReverseSoftLimitThreshold(kMinWristTicks, 10)
+            configForwardSoftLimitEnable(true, 10)
+            configReverseSoftLimitEnable(true, 10)
+        }
+    }
+
+    fun setPower(power: Double) {
+        val limitedPower = Utils.limit(power, 1.0)
+        var currentDegrees = mMaster.getSelectedSensorPosition() / kTicksPerDegree
+        if (currentDegrees > 90.0) {
+            setAngle(90.0)
+        } else if (currentDegrees < 0.0) {
+            setAngle(0.0)
+        } else {
+            mMaster.set(ControlMode.PercentOutput, limitedPower)
+        }
+    }
+
+    fun setAngle(angleDegrees: Double) {
+        var positionTicks = angleDegrees * kTicksPerDegree
+        positionTicks = Utils.limit(positionTicks, kMinWristTicks.toDouble(), kMaxWristTicks.toDouble())
+        mMaster.set(ControlMode.MotionMagic, positionTicks)
+    }
+
+    public override fun update() {
+    }
+
+    public override fun stop() {
+    }
+
+    public override fun reset() {
+    }
+}
