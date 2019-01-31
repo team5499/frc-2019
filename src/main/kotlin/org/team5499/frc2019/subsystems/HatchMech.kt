87f5472df20a3dcd4e0a3e5d7fc5239f3dadeae9
Team5499 <bhsrobot@gmail.com>
2019-01-31 15:57:18 -0800

    commit for pull

--- src/main/kotlin/org/team5499/frc2019/subsystems/HatchMech.kt
+++ src/main/kotlin/org/team5499/frc2019/subsystems/HatchMech.kt
@@ -15,42 +15,52 @@ public class HatchMech(
     val mHookMotor: LazyTalonSRX
 ) : Subsystem() {
 
-    public var isOpen: Boolean
+    private enum class ArmMode {
+
+    }
+
+    private enum class HookMode {
+        OPEN,
+        CLOSED
+    }
+
+    public val isOpen: Boolean
         get() {
             if (Constants.Hatch.OPEN_DETECTOR_NEGITIVE) {
-                return Constants.Hatch.HATCH_OPEN_DETECTION_LIMIT > mHookMotor.getSensorCollection().analogInRaw
+                return Constants.Hatch.HATCH_OPEN_DETECTION_LIMIT > mHookMotor.getSensorCollection().getAnalogInRaw()
             } else {
-                return mHookMotor.getSensorCollection().analogInRaw > Constants.Hatch.HATCH_OPEN_DETECTION_LIMIT
+                return mHookMotor.getSensorCollection().getAnalogInRaw() > Constants.Hatch.HATCH_OPEN_DETECTION_LIMIT
             }
         }
 
-    init
-    {
-        isOpen = false
-    }
 
     public fun setArmPercent(precent: Double) {
         mArmMotor.set(ControlMode.PercentOutput, precent)
     }
 
-    /**
-     * Open or close the latch
-     */
+    init {
+        mArmMotor.apply {
+            config_kP()
 
-    @Supress
+        }
+    }
+
+    @SuppressWarnings("MagicNumber")
     public fun setLatchState(shouldOpen: Boolean) {
         mHookMotor.setInverted(Constants.Hatch.OPEN_NEGIGIVE)
 
         if (shouldOpen) {
             @Suppress("MagicNumber")
-            mHookMotor.set(ControlMode.PercentOutput, 100.0)
+            mHookMotor.set(ControlMode.PercentOutput, 1.0)
         } else {
             @Suppress("MagicNumber")
-            mHookMotor.set(ControlMode.PercentOutput, -100.0)
+            mHookMotor.set(ControlMode.PercentOutput, -1.0)
         }
     }
 
-    public override fun update() {}
+    public override fun update() {
+        
+    }
 
     public override fun reset() {
         mHookMotor.setInverted(Constants.Hatch.OPEN_NEGIGIVE)
