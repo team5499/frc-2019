42cf195aab7963c4d23d2fdc8d3b99f24cf52286
Andy Cate <and164v@gmail.com>
2019-02-19 23:07:27 -0800

    Add code to detect flashdrive plugging

--- src/main/kotlin/org/team5499/frc2019/Robot.kt
+++ src/main/kotlin/org/team5499/frc2019/Robot.kt
@@ -1,14 +1,44 @@
 package org.team5499.frc2019
 
 import edu.wpi.first.wpilibj.TimedRobot
+import java.nio.file.WatchService
+import java.nio.file.StandardWatchEventKinds
+import java.nio.file.WatchKey
+import java.nio.file.Path
+import java.nio.file.Paths
+import java.nio.file.FileSystems
 
 @Suppress("MagicNumber")
 class Robot : TimedRobot(0.005) {
 
+    val watcher: WatchService
+    val keys: HashMap<WatchKey, Path>
+
+    init {
+        watcher = FileSystems.getDefault().newWatchService()
+        keys = HashMap<WatchKey, Path>()
+    }
+
+    @Suppress("MaxLineLength")
+    private fun register(dir: Path) {
+        val key = dir.register(watcher, StandardWatchEventKinds.ENTRY_CREATE, StandardWatchEventKinds.ENTRY_DELETE, StandardWatchEventKinds.ENTRY_MODIFY)
+        keys.put(key, dir)
+    }
+
     override fun robotInit() {
+        register(Paths.get("/media"))
+        println("registered")
     }
 
     override fun robotPeriodic() {
+        val event = watcher.poll()
+        if (event != null) {
+            println("event:")
+            println("$event")
+            event.pollEvents()
+            event.reset()
+            println("reset")
+        }
     }
 
     override fun disabledInit() {
