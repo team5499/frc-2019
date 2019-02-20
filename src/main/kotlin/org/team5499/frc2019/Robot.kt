7bfaf137f26042fc2215941feac6d10ddb524e34
Andy Cate <and164v@gmail.com>
2019-02-19 22:24:00 -0800

    revert to simple robot

--- src/main/kotlin/org/team5499/frc2019/Robot.kt
+++ src/main/kotlin/org/team5499/frc2019/Robot.kt
@@ -2,19 +2,13 @@ package org.team5499.frc2019
 
 import edu.wpi.first.wpilibj.TimedRobot
 
-import com.ctre.phoenix.motorcontrol.can.TalonSRX
-import com.ctre.phoenix.motorcontrol.FeedbackDevice
-
 @Suppress("MagicNumber")
 class Robot : TimedRobot(0.005) {
-    private val mTalon = TalonSRX(5)
 
     override fun robotInit() {
-        mTalon.configSelectedFeedbackSensor(FeedbackDevice.Analog)
     }
 
     override fun robotPeriodic() {
-        println("Hall effect value: ${mTalon.getSelectedSensorPosition()}")
     }
 
     override fun disabledInit() {
