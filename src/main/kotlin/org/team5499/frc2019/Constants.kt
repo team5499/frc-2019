f57b0c4ccee461f92e7cf0181a443060e41aa20c
Andy Cate <and164v@gmail.com>
2019-03-06 19:39:40 -0800

    Add code that should work with typed callbacks

--- src/main/kotlin/org/team5499/frc2019/Constants.kt
+++ src/main/kotlin/org/team5499/frc2019/Constants.kt
@@ -1,13 +1,26 @@
 package org.team5499.frc2019
 
 import org.team5499.dashboard.DashboardVar
+import org.team5499.dashboard.StringChooser
 
 @SuppressWarnings("MagicNumber")
 object Constants {
-    fun initConsts() {
-        DashboardVar.initClassProps(Constants::class)
+    fun initProps() {
+        println("init Constants")
     }
-    public var TEST_KP by DashboardVar(2.0)
-    public var TEST_KI by DashboardVar(1.0)
-    public var TEST_KD by DashboardVar(1.5)
+
+    public var AUTO_MODE = StringChooser("Constants.AUTO_MODE", "LEFT", "LEFT",
+                                                                        "MIDDLE",
+                                                                        "RIGHT")
+
+    object Lift {
+        fun initProps() {
+            println("init Lift")
+        }
+        public var KP by DashboardVar(2.0)
+        public var KI by DashboardVar(1.0)
+        public var KD by DashboardVar(1.5)
+
+        public var LOW_HEIGHT by DashboardVar(12.5)
+    }
 }
--- src/main/kotlin/org/team5499/frc2019/Robot.kt
+++ src/main/kotlin/org/team5499/frc2019/Robot.kt
@@ -8,23 +8,46 @@ import org.team5499.dashboard.Dashboard
 class Robot : TimedRobot(0.005) {
 
     override fun robotInit() {
-        Constants.initConsts()
         Dashboard.start(this, "config.json")
-        Dashboard.addVarListener("Constants.TEST_KP", {
-            key: String, value: Any? ->
-            println("KP: $value")
-        })
-        Dashboard.addVarListener("Constants.TEST_KI", {
-            key: String, value: Any? ->
-            println("KI: $value")
-        })
-        Dashboard.addVarListener("Constants.TEST_KD", {
-            key: String, value: Any? ->
-            println("KD: $value")
-        })
+        Constants.initProps()
+
+        Constants.AUTO_MODE.addInlineListener() {
+            key: String, value: String ->
+            println("key: $key, selected option: $value")
+        }
+        Dashboard.addInlineListener("Constants.Lift.KP") {
+            key: String, value: Double? ->
+            if (value != null) {
+                // set the kP value here
+                println("key: $key, kP: $value")
+            }
+        }
+        Dashboard.addInlineListener("Constants.Lift.KI") {
+            key: String, value: Double? ->
+            if (value != null) {
+                // set the kI value here
+                println("key: $key, kI: $value")
+            }
+        }
+        Dashboard.addInlineListener("Constants.Lift.KD") {
+            key: String, value: Double? ->
+            if (value != null) {
+                // set the kD value here
+                println("key: $key, kD: $value")
+            }
+        }
+
+        Dashboard.addInlineListener("Constants.Lift.LOW_HEIGHT") {
+            key: String, value: Double? ->
+            // set the low height
+            if (value != null) {
+                println("key: $key, low height: $value")
+            }
+        }
     }
 
     override fun robotPeriodic() {
+        Dashboard.update() // has to be called in a loop
     }
 
     override fun disabledInit() {
