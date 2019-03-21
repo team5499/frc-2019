c02f9f794850d713781fc365c27fb5c2f46cfcad
Ben Scholar <bens@digitalman.com>
2019-03-20 21:28:07 -0700

    yeet

--- src/main/kotlin/org/team5499/frc2019/subsystems/Vision.kt
+++ src/main/kotlin/org/team5499/frc2019/subsystems/Vision.kt
@@ -16,40 +16,40 @@ public class Vision : Subsystem() {
     @Suppress("MagicNumber")
     public enum class VisionMode(val value: Int) { DRIVER(9), VISION(0) } // pipeline
 
-    private val mTable = NetworkTableInstance.getDefault.getTable("limelight")
+    private val mTable = NetworkTableInstance.getDefault().getTable("limelight")
 
     public var ledState: LEDState = LEDState.OFF
         set(value) {
-            table.getEntry("ledMode").setNumber(value.value)
+            mTable.getEntry("ledMode").setNumber(value.value)
             field = value
         }
     public var visionMode: VisionMode = VisionMode.VISION
         set(value) {
-            table.getEntry("pipeline").setNumber(value.value)
+            mTable.getEntry("pipeline").setNumber(value.value)
             field = value
         }
     public val hasValidTarget: Boolean
         get() {
             return if (
-                table.getEntry("tv").getDouble(0.0) == 1.0
+                mTable.getEntry("tv").getDouble(0.0) == 1.0
             ) true
             else false
         }
     public val targetXOffset: Double
         get() {
-            return table.getEntry("tx").getDouble(0.0)
+            return mTable.getEntry("tx").getDouble(0.0)
         }
     public val targetYOffset: Double
         get() {
-            return table.getEntry("ty").getDouble(0.0)
+            return mTable.getEntry("ty").getDouble(0.0)
         }
     public val targetSkew: Double
         get() {
-            return table.getEntry("ts").getDouble(0.0)
+            return mTable.getEntry("ts").getDouble(0.0)
         }
     public val targetArea: Double
         get() {
-            return table.getEntry("ta").getDouble(0.0)
+            return mTable.getEntry("ta").getDouble(0.0)
         }
 
     // http://docs.limelightvision.io/en/latest/cs_estimating_distance.html
@@ -57,10 +57,9 @@ public class Vision : Subsystem() {
         get() {
             // return (Constants.Vision.HATCH_TARGET_HEIGHT - Constants.Vision.CAMERA_HEIGHT) /
             //     tan(Constants.Vision.CAMERA_VERTICAL_ANGLE + targetYOffset)
-            val table = NetworkTableInstance.getDefault().getTable("limelight")
-                .getEntry("camtran").getDoubleArray(arrayOf<Double>(0.0, 0.0, 0.0, 0.0, 0.0, 0.0))
-            val x = table[0]
-            val z = table[2]
+            val array = mTable.getEntry("camtran").getDoubleArray(arrayOf<Double>(0.0, 0.0, 0.0, 0.0, 0.0, 0.0))
+            val x = array[0]
+            val z = array[2]
             println("vision coords: $x, $z")
             return Math.hypot(x, z)
         }
@@ -71,9 +70,9 @@ public class Vision : Subsystem() {
         }
 
     init {
-        table.getEntry("camMode").setNumber(0)
-        table.getEntry("stream").setNumber(0)
-        table.getEntry("ledMode").setNumber(0)
+        mTable.getEntry("camMode").setNumber(0)
+        mTable.getEntry("stream").setNumber(0)
+        mTable.getEntry("ledMode").setNumber(0)
     }
 
     public override fun update() {}
